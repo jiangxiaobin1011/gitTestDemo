08f9ad1e5b406077bdf64167d31e9ffe2965114c
jiangbin <1029194438@qq.com>
2021-06-08 11:10:05 +0800

    Squashed commit of the following:
    commit ef8c50227aa6175917de3374c94b870108d33a6e
    Author: jiangbin <1029194438@qq.com>
    Date:   Tue Jun 8 10:06:45 2021 +0800
    
        完成future4
    
    commit 44e7f5c300916b2b0b0a04fc7d9600a9bd4cdaff
    Author: jiangbin <1029194438@qq.com>
    Date:   Mon Jun 7 17:20:03 2021 +0800
    
        完成功能2、3
    
    commit 24358438f98546fbda1c5c8c255aa0524f57f323
    Author: jiangbin <1029194438@qq.com>
    Date:   Mon Jun 7 17:16:04 2021 +0800
    
        完成 future1

--- GItDemo/GItDemo/ViewController.m
+++ GItDemo/GItDemo/ViewController.m
@@ -15,7 +15,19 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+    //future1 功能完成
+    UIView* v;
+    
+    //future2 功能完成
+    UIView* v2;
+    
+    //future3 功能完成
+    UIView* v3;
     // Do any additional setup after loading the view.
+    
+    //future4 功能完成
+    UIView* v4;
+    
 }
 
 
